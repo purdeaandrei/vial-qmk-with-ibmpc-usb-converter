5e37ae1338c6ec45960af65b85bb5c6193fddbc5
Markus Fritsche <mfritsche@reauktion.de>
2021-08-04 21:58:08 +0200

    pragma once

--- keyboards/converter/ibmpc_usb/config.h
+++ keyboards/converter/ibmpc_usb/config.h
@@ -16,9 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
-
+#pragma once
 
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6536
