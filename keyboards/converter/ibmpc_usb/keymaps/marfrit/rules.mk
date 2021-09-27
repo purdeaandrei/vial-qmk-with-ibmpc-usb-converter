edd039123016215edc9432e5cdc785f7a8f89969
Markus Fritsche <fritsche.markus@gmail.com>
2021-09-27 16:40:58 +0200

    rules

--- keyboards/converter/ibmpc_usb/keymaps/marfrit/rules.mk
+++ keyboards/converter/ibmpc_usb/keymaps/marfrit/rules.mk
@@ -1,5 +1,6 @@
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover
