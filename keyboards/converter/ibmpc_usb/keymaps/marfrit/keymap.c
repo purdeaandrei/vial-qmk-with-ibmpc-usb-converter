37d0c9cee8830a0b869a3d8ce0218e7cecd28d6d
Markus Fritsche <fritsche.markus@gmail.com>
2021-11-16 10:23:27 +0000

    adjustments for unicode

--- keyboards/converter/ibmpc_usb/keymaps/marfrit/keymap.c
+++ keyboards/converter/ibmpc_usb/keymaps/marfrit/keymap.c
@@ -17,6 +17,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+enum unicode_names {
+    KC_AU,
+    KC_aU,
+    KC_OU,
+    KC_oU,
+    KC_UU,
+    KC_uU,
+    KC_SS,
+    KC_sS,
+    KC_EUR
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [KC_AU]  = 0xC4,  // Ä
+    [KC_aU]  = 0xE4,  // ä
+    [KC_OU]  = 0xD6,
+    [KC_oU]  = 0xF6,
+    [KC_UU]  = 0xDC,
+    [KC_uU]  = 0xFC,
+    [KC_SS]  = 0x1E9E,
+    [KC_sS]  = 0xDF,
+    [KC_EUR] = 0x20AC
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 122 keys, but 127 matrix positions (stabilizer inserts can be
     replaced by flipper an converted to keys) */
@@ -25,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24,
                         KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
 
-    RESET  ,KC_ESC,     KC_GRV,  KC_1,        KC_2,         KC_3,         KC_4,         KC_5,    KC_6,    KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS, KC_EQL,  KC_NUHS, KC_BSPC,    KC_INS,  KC_HOME,KC_PGUP,    KC_NLCK,   KC_PSLS,KC_PAST,KC_PMNS,
-    RESET,  KC_MPLY,    KC_TAB,  ALL_T(KC_K), KC_U,         KC_Q,         KC_DOT,       KC_J,    KC_V,    KC_G,         KC_C,         KC_L,         KC_F,         KC_LBRC, KC_RBRC,          KC_SYSREQ,  KC_DEL,  KC_END, KC_PGDN,    KC_P7,     KC_P8,  KC_P9,  KC_PPLS,
-    KC_PAUS,KC_INT5,    KC_LCTL, KC_H,        LALT_T(KC_I), LCTL_T(KC_E), LSFT_T(KC_A), KC_O,    KC_D,    RSFT_T(KC_T), RCTL_T(KC_R), LALT_T(KC_N), KC_S,         KC_QUOT, KC_BSLS, KC_ENT,                       KC_UP,              KC_P4,     KC_P5,  KC_P6,  KC_PCMM,
-    KC_APP, KC_PSCR,    KC_LSFT, KC_NUBS,     KC_X,         RALT_T(KC_Y), KC_SCLN,      KC_COMM, KC_SLSH, KC_B,         KC_P,         KC_W,         RALT_T(KC_M), KC_Z,    KC_NO,   KC_RSFT,             KC_LEFT, KC_INT2,KC_RGHT,    KC_P1,     KC_P2,  KC_P3,  KC_PENT,
-    KC_RGUI,KC_LGUI,    KC_CAPS,              KC_LALT,                                           KC_SPC,                                            KC_RALT,                        KC_RCTL,                      KC_DOWN,            KC_SYSREQ, KC_P0,  KC_PDOT,KC_NO
+    RESET  ,KC_ESC,     ALL_T(KC_GRV), KC_1,    KC_2,         KC_3,         KC_4,         KC_5,    KC_6,    KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS, KC_EQL,  KC_NUHS, KC_BSPC,    KC_INS,  KC_HOME,KC_PGUP,    KC_NLCK,   KC_PSLS,KC_PAST,KC_PMNS,
+    RESET,  KC_MPLY,    KC_TAB,        KC_K,    KC_U,         KC_Q,         KC_DOT,       KC_J,    KC_V,    KC_G,         KC_C,         KC_L,         KC_F,         KC_LBRC, KC_RBRC,          KC_SYSREQ,  KC_DEL,  KC_END, KC_PGDN,    KC_P7,     KC_P8,  KC_P9,  KC_PPLS,
+    KC_PAUS,KC_INT5,    KC_LCTL,       KC_H,    LALT_T(KC_I), LCTL_T(KC_E), LSFT_T(KC_A), KC_O,    KC_D,    RSFT_T(KC_T), RCTL_T(KC_R), LALT_T(KC_N), KC_S,         KC_QUOT, KC_BSLS, KC_ENT,                       KC_UP,              KC_P4,     KC_P5,  KC_P6,  KC_PCMM,
+    KC_APP, KC_PSCR,    KC_LSFT,       KC_NUBS, KC_X,         RALT_T(KC_Y), KC_SCLN,      KC_COMM, KC_SLSH, KC_B,         KC_P,         KC_W,         RALT_T(KC_M), KC_Z,    KC_NO,   KC_RSFT,             KC_LEFT, KC_INT2,KC_RGHT,    KC_P1,     KC_P2,  KC_P3,  KC_PENT,
+    KC_RGUI,KC_LGUI,    KC_CAPS,                KC_LALT,                                           KC_SPC,                                            KC_RALT,                        KC_RCTL,                      KC_DOWN,            KC_SYSREQ, KC_P0,  KC_PDOT,KC_NO
 
     ),
 /*
--- keyboards/converter/ibmpc_usb/keymaps/marfrit/rules.mk
+++ keyboards/converter/ibmpc_usb/keymaps/marfrit/rules.mk
@@ -1,10 +1,20 @@
+# IBMPC Options
+IBMPC_SECONDARY = no		# enable secondary interface
+IBMPC_MOUSE_ENABLE = no   	# enable mouse support
+LTO_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
+LTO_ENABLE = yes
+BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
+MIDI_ENABLE = no             # MIDI controls
+AUDIO_ENABLE = no            # Audio output on port C6
+UNICODE_ENABLE = no          # Unicode
+BLUETOOTH_ENABLE = no        # Enable Bluetooth with the Adafruit EZ-Key HID
+RGBLIGHT_ENABLE = no         # Enable WS2812 RGB underlight.
+API_SYSEX_ENABLE = no
+UNICODEMAP_ENABLE = yes
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover
-# IBMPC Options
-IBMPC_SECONDARY = no		# enable secondary interface
-IBMPC_MOUSE_ENABLE = no   	# enable mouse support
-LTO_ENABLE = yes
