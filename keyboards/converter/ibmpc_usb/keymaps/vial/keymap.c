14ec1e6dc22089a7eaceaea320dc1e5d961622b5
Purdea Andrei <andrei@purdea.ro>
2022-10-08 22:51:28 +0300

    Changed default keymap to better match TMK default keymap (MO(1) key changed to LT(1,kC_APP) key)

--- keyboards/converter/ibmpc_usb/keymaps/vial/keymap.c
+++ keyboards/converter/ibmpc_usb/keymaps/vial/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,        KC_BSLS,     KC_DEL, KC_END, KC_PGDN,    KC_P7,  KC_P8,  KC_P9,  KC_PPLS,
     KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,        KC_NUHS,KC_ENT,                                  KC_P4,  KC_P5,  KC_P6,  KC_PCMM,
     KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,        KC_RO,  KC_RSFT,             KC_UP,              KC_P1,  KC_P2,  KC_P3,  KC_PENT,
-    KC_LCTL,KC_LGUI,KC_LALT,KC_MHEN,                KC_SPC,                 KC_HENK,KC_KANA,KC_RALT,KC_RGUI,MO(1),  KC_RCTL,     KC_LEFT,KC_DOWN,KC_RGHT,            KC_P0,  KC_PDOT,KC_PEQL
+    KC_LCTL,KC_LGUI,KC_LALT,KC_MHEN,                KC_SPC,                 KC_HENK,KC_KANA,KC_RALT,KC_RGUI,LT(1,KC_APP),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT,            KC_P0,  KC_PDOT,KC_PEQL
     ),
     LAYOUT(
                     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
